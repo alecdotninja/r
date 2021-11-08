00becec5fe4eb37a3dca79243f4af6d50d074513
Alec Larsen <hello@alec.ninja>
2021-11-07 23:35:52 -0800

    cleanup

--- src/ownership.rs
+++ src/ownership.rs
@@ -1,20 +1,22 @@
 use core::ops::{Add, Div, Mul};
-
 use typenum::consts::{B1, U1, U2, U4, U8};
-use typenum::{op, UInt, Gcd, Unsigned, NonZero, PowerOfTwo};
+use typenum::{op, Gcd, NonZero, PowerOfTwo, UInt, Unsigned};
 
 pub trait Odd {}
 impl<U: Unsigned> Odd for UInt<U, B1> {}
 
-pub struct F<P, Q>(P, Q)
-where
-    P: Unsigned + NonZero + Odd,
-    Q: Unsigned + NonZero + PowerOfTwo;
+pub trait Num: Unsigned + NonZero + Odd {}
+impl<T: Unsigned + NonZero + Odd> Num for T {}
 
-pub type Full       = F<U1, U1>;
-pub type Half       = F<U1, U2>;
-pub type Quarter    = F<U1, U4>;
-pub type Eighth     = F<U1, U8>;
+pub trait Den: Unsigned + NonZero + PowerOfTwo {}
+impl<T: Unsigned + NonZero + PowerOfTwo> Den for T {}
+
+pub struct F<P: Num, Q: Den>(P, Q);
+
+pub type Full = F<U1, U1>;
+pub type Half = F<U1, U2>;
+pub type Quarter = F<U1, U4>;
+pub type Eighth = F<U1, U8>;
 
 pub unsafe trait Ownership {
     const IS_FULL: bool;
@@ -28,61 +30,48 @@ pub unsafe trait JoinsWith<Other: Ownership>: Ownership {
     type Joined: Ownership;
 }
 
-unsafe impl<P, Q> Ownership for F<P, Q>
-where
-    P: Unsigned + NonZero + Odd,
-    Q: Unsigned + NonZero + PowerOfTwo,
-{
+unsafe impl<P: Num, Q: Den> Ownership for F<P, Q> {
     const IS_FULL: bool = (P::USIZE == 1usize && Q::USIZE == 1usize);
 }
 
-unsafe impl<P, Q> CanSplit for F<P, Q>
+unsafe impl<P: Num, Q: Den> CanSplit for F<P, Q>
 where
-    P: Unsigned + NonZero + Odd,
-    Q: Unsigned + NonZero + PowerOfTwo + Mul<U2>,
-    op!(Q * U2): Unsigned + NonZero + PowerOfTwo,
+    Q: Mul<U2>,
+    op!(Q * U2): Den,
 {
     type Split = F<P, op!(Q * U2)>;
 }
 
-type InproperNumerator<P, Q, R, S> =
-    op!(P * S + R * Q);
+type InproperNum<P, Q, R, S> = op!(P * S + R * Q);
 
-type InproperDenominator<Q, S> =
-    op!(Q * S);
+type InproperDen<Q, S> = op!(Q * S);
 
-type ReducingFactor<P, Q, R, S> =
-    <InproperNumerator<P, Q, R, S> as Gcd<InproperDenominator<Q, S>>>::Output;
+type ReduceFactor<P, Q, R, S> = <InproperNum<P, Q, R, S> as Gcd<InproperDen<Q, S>>>::Output;
 
-type ReducedNumerator<P, Q, R, S> =
-    <InproperNumerator<P, Q, R, S> as Div<ReducingFactor<P, Q, R, S>>>::Output;
+type ReducedNum<P, Q, R, S> = <InproperNum<P, Q, R, S> as Div<ReduceFactor<P, Q, R, S>>>::Output;
 
-type ReducedDenominator<P, Q, R, S> =
-    <InproperDenominator<Q, S> as Div<ReducingFactor<P, Q, R, S>>>::Output;
+type ReducedDen<P, Q, R, S> = <InproperDen<Q, S> as Div<ReduceFactor<P, Q, R, S>>>::Output;
 
-unsafe impl<P, Q, R, S> JoinsWith<F<R, S>> for F<P, Q>
+unsafe impl<P: Num, Q: Den, R: Num, S: Den> JoinsWith<F<R, S>> for F<P, Q>
 where
-    P: Unsigned + NonZero + Odd + Mul<S>,
-    Q: Unsigned + NonZero + PowerOfTwo + Mul<S>,
-    R: Unsigned + NonZero + Odd + Mul<Q>,
-    S: Unsigned + NonZero + PowerOfTwo + Mul<Q>,
-
-    // It is possible to create `InproperNumerator`
+    // It is possible to create `InproperNum`
+    P: Mul<S>,
+    R: Mul<Q>,
     op!(P * S): Add<op!(R * Q)>,
 
-    // It is possible to create `ReducingFactor`
-    InproperNumerator<P, Q, R, S>: Gcd<InproperDenominator<Q, S>>,
+    // It is possible to create `InproperDen`
+    Q: Mul<S>,
 
-    // It is possible to create `ReducedNumerator` and `ReducedDenominator`
-    InproperNumerator<P, Q, R, S>: Div<ReducingFactor<P, Q, R, S>>,    
-    InproperDenominator<Q, S>: Div<ReducingFactor<P, Q, R, S>>,
+    // It is possible to create `ReduceFactor`
+    InproperNum<P, Q, R, S>: Gcd<InproperDen<Q, S>>,
 
-    // It is possible to use `ReducedNumerator` and `ReducedDenominator` in `F`
-    ReducedNumerator<P, Q, R, S>: Unsigned + NonZero + Odd,
-    ReducedDenominator<P, Q, R, S>: Unsigned + NonZero + PowerOfTwo,
+    // It is possible to create `ReducedNum` and `ReducedDen`
+    InproperNum<P, Q, R, S>: Div<ReduceFactor<P, Q, R, S>>,
+    InproperDen<Q, S>: Div<ReduceFactor<P, Q, R, S>>,
+
+    // `ReducedNum` and `ReducedDen` can be used in `F`
+    ReducedNum<P, Q, R, S>: Unsigned + NonZero + Odd,
+    ReducedDen<P, Q, R, S>: Unsigned + NonZero + PowerOfTwo,
 {
-    type Joined = F<
-        ReducedNumerator<P, Q, R, S>,
-        ReducedDenominator<P, Q, R, S>,
-    >;
+    type Joined = F<ReducedNum<P, Q, R, S>, ReducedDen<P, Q, R, S>>;
 }
